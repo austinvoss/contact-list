ae259f0650aa896dced064d439c993de4f0a31bd
Esteban Ordonez <estebanusco@gmail.com>
2023-06-26 19:17:08 -0700

    Update ContactList.jsx
    Deleting dummy data and set useState([])

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -4,12 +4,8 @@ import ContactRow from "./ContactRow";
 import SelectedContact from "./SelectedContact";
 
 export default function ContactList({ setSelectedContactId }) {
-  const dummyContacts = [
-    { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
-    { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
-    { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
-  ];
-  const [contacts, setContacts] = useState(dummyContacts);
+  
+  const [contacts, setContacts] = useState([]);
 
   useEffect(() => {
     async function fetchContacts() {
@@ -18,7 +14,7 @@ export default function ContactList({ setSelectedContactId }) {
           "https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users"
         );
         const result = await response.json();
-        console.log(result);
+        
         setContacts(result);
       } catch (error) {
         console.error(error);
